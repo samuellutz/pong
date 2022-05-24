b6358f374cce9604a97e235e6e4283afe2f937b7
samuel lutz <samlutz89@gmail.com>
2022-05-24 08:52:48 -0700

    setting up paddle movement

---
+++ paddle.js
@@ -0,0 +1,5 @@
+export default class Paddle {
+    constructor(paddleEl){
+        this.paddleEl = paddleEl;
+    }
+}
--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 import Ball from "./ball.js";
+import Paddle from "./paddle.js";
 
 const ball = new Ball(document.getElementById("ball"))
+const playerPaddle = new paddle(document.getElementById("player-Paddle"))
+const computerPaddle = new paddle(document.getElementById("computer-Paddle"))
 
 let lastTime
 
@@ -13,5 +16,9 @@ function update(time) {
     lastTime = time;
     window.requestAnimationFrame(update)
 }
+
+document.addEventListener("mousemove", e => {
+    playerPaddle.position = e.y / window.innerHeight * 100
+})
 
 window.requestAnimationFrame(update)
